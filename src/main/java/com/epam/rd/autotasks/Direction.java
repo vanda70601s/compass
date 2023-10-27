15ebaaba4517e6ada654159cff9ae848fb9f4e80
Work <vanda70601s@mail.ru>
2023-10-27 22:11:38 +0300

    Done

--- src/main/java/com/epam/rd/autotasks/Direction.java
+++ src/main/java/com/epam/rd/autotasks/Direction.java
@@ -1,6 +1,5 @@
 package com.epam.rd.autotasks;
 
-import java.util.Optional;
 
 public enum Direction {
     N(0),
@@ -16,46 +15,51 @@ public enum Direction {
         this.degrees = degrees;
     }
 
-    public int getDegrees() {
-        return degrees;
-    }
-
     private final int degrees;
 
     public static Direction ofDegrees(int degrees) {
         for (Direction direction : Direction.values()) {
-            if(degrees == direction.degrees || degrees % 360 == direction.degrees || degrees + 360 == direction.degrees)
+            if (degrees == direction.degrees || degrees % 360 == direction.degrees || degrees + 360 == direction.degrees)
                 return direction;
-        }return null;
+        }
+        return null;
     }
 
     public static Direction closestToDegrees(int degrees) {
         degrees = degrees % 360;
         int deg1 = degrees / 45;
         int deg2 = degrees % 45;
-        if(deg2 <= 23) return ofDegrees(45*deg1);
-        else return ofDegrees(45*(deg1+1));
+        if (deg2 <= 23) return ofDegrees(45 * deg1);
+        else return ofDegrees(45 * (deg1 + 1));
     }
 
     public Direction opposite() {
-        switch (this){
-            case N: return S;
-            case NE: return SW;
-            case E: return W;
-            case SE: return NW;
-            case S: return N;
-            case SW: return NE;
-            case W: return E;
-            case NW: return SE;
+        switch (this) {
+            case N:
+                return S;
+            case NE:
+                return SW;
+            case E:
+                return W;
+            case SE:
+                return NW;
+            case S:
+                return N;
+            case SW:
+                return NE;
+            case W:
+                return E;
+            case NW:
+                return SE;
         }
         return null;
     }
 
     public int differenceDegreesTo(Direction direction) {
-        if(this == N && direction.degrees> 180) {
+        if (this == N && direction.degrees > 180) {
             return Math.abs(360 - direction.degrees);
         }
-        if(direction ==N && this.degrees>180){
+        if (direction == N && this.degrees > 180) {
             return Math.abs(360 - this.degrees);
 
         }
